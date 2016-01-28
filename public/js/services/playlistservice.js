4ba6c4adc9e6333a221c92647453109c8c2836fd
adamking81@hotmail.com <mangakid@users.noreply.github.com>
2016-01-28 20:16:00 +0100

    non explicit playlist generation improvement

--- public/js/services/playlistservice.js
+++ public/js/services/playlistservice.js
@@ -12,29 +12,30 @@ app.factory('playlistservice', ['spotifycalls', '$q', function(spotifycalls, $q)
 
 		    var tempNumberOfTracks = numOfTracks;
 
+		    if(!explicitAllowed){
+		    	var nonExplicitArray = [];
+		    	for(i = 0; i <data.tracks.length; i++){
+		    		if(data.tracks[i].explicit === false){
+		    			nonExplicitArray.push(data.tracks[i]);
+		    		}
+		    	}
+		    	data.tracks = nonExplicitArray;
+		    }
+
 		    if(numOfTracks > data.tracks.length){
 		      tempNumberOfTracks = data.tracks.length;
 		    }
 		    for(var i = 0; i < tempNumberOfTracks; i++){
 
-		        var newTrack = {artist:"",title:"",spotifyId:""};
-		        var isExplicit = data.tracks[i].explicit;
+				var newTrack = {artist:"",title:"",spotifyId:""};
 
-		        newTrack.title = data.tracks[i].name;
-		        newTrack.spotifyId = data.tracks[i].uri;
-		        newTrack.artist = data.tracks[i].artists[0].name;
-		        //console.log("isExplicit? " + data.tracks[i].explicit);
-		        if(isExplicit === explicitAllowed)
-		        {
-		          //console.log("track explicit and explicit checkbox = true");
-		          newTracks.push(newTrack);
-		          trackUris.push(data.tracks[i].uri);
-		        } 
-		        else if(isExplicit === false){
-		          //console.log("track not explicit");
-		          newTracks.push(newTrack);
-		          trackUris.push(data.tracks[i].uri);
-		        }
+				newTrack.title = data.tracks[i].name;
+				newTrack.spotifyId = data.tracks[i].uri;
+				newTrack.artist = data.tracks[i].artists[0].name;
+
+				newTracks.push(newTrack);
+				trackUris.push(data.tracks[i].uri);
+
 		    }
 		    processedArray.push(newTracks);
 		    processedArray.push(trackUris);
